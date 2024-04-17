b97f4f920c8c2a522df3087d39f2bd3de077bbb4
anandpothraj <anandpothraj11052001@gmail.com>
2024-04-17 19:46:18 +0530

    solved 1

---
+++ leetcode_1.js
@@ -0,0 +1,18 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+var twoSum = function(nums, target) {
+    for(let i=0; i < nums.length; i++){
+        for(let j=i+1; j < nums.length; j++){
+            if(nums[j]==target-nums[i]){
+                return [i,j]
+            }
+        }
+    }
+    return null;
+};
+
+console.log(twoSum([2,7,11,15],9)); // Output: [0,1]
+console.log(twoSum([3,2,4],6)); // Output: [1,2]
+console.log(twoSum([3,3],6)); // Output: [0,1]
