f582adf66e2d3de4f9b1e304da68900b570a13da
anandpothraj <anandpothraj11052001@gmail.com>
2024-04-18 12:56:07 +0530

    Solved 35

---
+++ leetcode_35.js
@@ -0,0 +1,31 @@
+// 35. Search Insert Position
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+// You must write an algorithm with O(log n) runtime complexity.
+
+var searchInsert = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while(left<=right){
+
+        let mid = Math.floor((left + right)/2);
+
+        if( nums[mid] === target){
+            return mid;
+        }
+
+        if( target > nums[mid]){
+            left = mid + 1;
+        }
+        else{
+            right = mid - 1
+        }
+    }
+    return left;
+};
+
+
+console.log(searchInsert([1,3,5,6],5))  // Output : 2.
+console.log(searchInsert([1,3,5,6],2))  // Output : 1.
+console.log(searchInsert([1,3,5,6],7))  // Output : 4.
+console.log(searchInsert([1,3],2))  // Output : 1.
