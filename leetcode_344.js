fa515a42a5091535b3b3f3d5e0e4a2ea6175d2cb
anandpothraj <anandpothraj11052001@gmail.com>
2024-05-27 19:09:35 +0530

    Solved leetcode 344

---
+++ leetcode_344.js
@@ -0,0 +1,18 @@
+// 344. Reverse String
+
+// Write a function that reverses a string. The input string is given as an array of characters s.
+// You must do this by modifying the input array in-place with O(1) extra memory.
+
+var reverseString = function(s) {
+    let left = 0;
+    let right = s.length - 1;
+    while(left<=right){
+        [ s[left], s[right ] ] = [ s[right], s[left ] ];
+        left++;
+        right--;
+    }
+    return s;
+};
+
+console.log(reverseString(["h","e","l","l","o"]));           // ["o","l","l","e","h"]
+console.log(reverseString(["H","a","n","n","a","h"]));       // ["h","a","n","n","a","H"]
