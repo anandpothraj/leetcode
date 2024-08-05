460b1682c83b179f428676bb9605586f8f677af1
anandpothraj <anandpothraj11052001@gmail.com>
2024-08-05 17:44:43 +0530

    Optimized 27th solution

--- leetcode_27.js
+++ leetcode_27.js
@@ -5,14 +5,13 @@
 // Return k.
 
 var removeElement = function(nums, val) {
-    for(let i=0; i<nums.length; i++){
+    for(let i=nums.length-1; i>=0; i--){
         if(nums[i] === val){
             nums.splice(i,1);
-            i--;
         }
     }
     return nums.length;
 };
 
-console.log(removeElement([3,2,2,3],3));  // Output : 2.
-console.log(removeElement([0,1,2,2,3,0,4,2],2));  // Output : 5.
+console.log(removeElement([3,2,2,3],3));          // 2.
+console.log(removeElement([0,1,2,2,3,0,4,2],2));  // 5.
