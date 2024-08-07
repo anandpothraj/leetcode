6e04d0a34822339a7911c9ef98490e95b5e8bc30
anandpothraj <anandpothraj11052001@gmail.com>
2024-08-07 21:19:45 +0530

    Revised 27th question

--- leetcode_27.js
+++ leetcode_27.js
@@ -5,12 +5,14 @@
 // Return k.
 
 var removeElement = function(nums, val) {
-    for(let i=nums.length-1; i>=0; i--){
-        if(nums[i] === val){
-            nums.splice(i,1);
+    let index = 0;
+    for(let i=0; i<nums.length; i++){
+        if(nums[i] !== val){
+            nums[index] = nums[i];
+            index++;
         }
     }
-    return nums.length;
+    return index;
 };
 
 console.log(removeElement([3,2,2,3],3));          // 2.
